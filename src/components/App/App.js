fb06647558d2967dc6e8f007594b45a4d7e107df
DeeKayMA <100499813+DeeKayMA@users.noreply.github.com>
2025-03-24 13:07:23 +0000

    Written API calls for getRedditAccessToken, getBestPosts, getPostComments

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,24 @@
-import logo from '../../logo.svg';
 import './App.css';
+import {useState, useEffect} from "react";
 import Navbar from '../Navbar/navbar';
 import Sidebar from '../Sidebar/sidebar';
 import Content from '../content/content';
 
+
 function App() {
+  const [selectedSubreddit, setSelectedSubreddit] = useState(null);
+  const [posts, setPosts] = useState([]);
+
+  // Fetch posts whenever the selected subreddit changes
+  // pass the posts to the Content component
+  // pass the subreddit to the Sidebar component
+
+  //The list of Subreddits I want to show in the sidebar
+  const getSubreddits = () => {
+    return ['javascript', 'reactjs', 'webdev', 'programming', 'node', 'css', 'frontend', 'web_design', 'learnprogramming', 'design'];
+  };
+
+  
   return (
     <div className="App">
       <Navbar className="navbar"/>
---
+++ src/utils/redditAPI.js
@@ -0,0 +1,104 @@
+import fetch from "node-fetch"; 
+import dotenv from "dotenv";
+
+dotenv.config();
+
+
+// API call to get Reddit Access Token
+
+export const getRedditAccessToken = async () => {
+
+    const clientId = process.env.REDDIT_CLIENT_ID;
+    const clientSecret = process.env.REDDIT_CLIENT_SECRET;
+
+    const auth = btoa(`${clientId}:${clientSecret}`)
+    const tokenUrl = 'https://www.reddit.com/api/v1/access_token';
+
+    try {
+
+        const response = await fetch(tokenUrl, {
+            method: 'POST',
+            headers: {
+                'Authorization': `Basic ${auth}`,
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: new URLSearchParams({
+                grant_type: 'client_credentials'
+            })
+        });
+
+        if(!response.ok){
+            throw new Error('Failed to fetch token:', response.statusText)
+        }
+
+        const data = await response.json();
+
+        const accessToken = data.access_token;
+
+        console.log(`Access Token: ${accessToken}`);
+        return (accessToken)
+
+    } catch (error) {
+        console.error('Error getting access token:', error);
+        return (null);
+    }
+};
+
+//API call to get top 10 posts for each subreddit
+
+export const getBestPosts = async (subreddit) => {
+    const accessToken = await getRedditAccessToken();
+    const postUrl = `https://oauth.reddit.com/r/${subreddit}/best?limit=10`
+
+    try {
+        const response = await fetch(postUrl, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${accessToken}`,
+            },
+        });
+
+        if(!response.ok){
+            throw new Error(`Failed to fetch posts`)
+        }
+
+        const data = await response.json();
+        return data.data.children; 
+
+    } catch (error) {
+        console.error('Error getting posts:', error);
+        return (null);
+    }
+
+};
+
+
+// API call to get top 10 comments for each post
+
+export const getPostComments = async (subreddit, postId) => {
+    const accessToken = await getRedditAccessToken();
+    const commentsUrl = `https://oauth.reddit.com/[/r/${subreddit}/comments/${postId}?limit=10?depth=1`
+
+    try {
+
+        const response = await fetch(commentsUrl, {
+            method: 'GET',
+            headers: {
+                'Authorization' : `Bearer ${accessToken}`,
+            },
+        });
+
+        if(!response.ok){
+            throw new Error(`Failed to fetch post comments tree`)
+        }
+
+        const data = await response.json();
+        return data[1].data.children;         
+        
+    } catch (error) {
+        console.error('Error getting post comments tree:', error);
+        return (null);
+    }
+};
+
+
